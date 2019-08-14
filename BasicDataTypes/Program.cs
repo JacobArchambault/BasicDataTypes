2f5be4daf9fe41c44a0dd1be5ef37620abb936e3
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-14 09:40:01 -0400

    Create data types.

--- BasicDataTypes/Program.cs
+++ BasicDataTypes/Program.cs
@@ -8,8 +8,29 @@ namespace BasicDataTypes
 {
     class Program
     {
+        static void LocalVarDeclarations()
+        {
+            Console.WriteLine("=> Data Declarations:");
+            // Local variables are declared and initialized as follows: 
+            // dataType varName = initialValue;
+            int myInt = 0;
+
+            string myString;
+            myString = "This is my character data";
+
+            // Declare 3 bools on a single line.
+            bool b1 = true, b2 = false, b3 = b1;
+
+            // Use System.Boolean data type to declare a bool.
+            System.Boolean b4 = false;
+            Console.WriteLine("Your data: {0}, {1}, {2}, {3}, {4}, {5}",
+                myInt, myString, b1, b2, b3, b4);
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
+            LocalVarDeclarations();
+            Console.ReadLine();
         }
     }
 }
