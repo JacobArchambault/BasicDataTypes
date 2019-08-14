47b0a43867b9781ac79712178a59618e2382a679
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-14 10:22:53 -0400

    Add bigInteger

--- BasicDataTypes/Program.cs
+++ BasicDataTypes/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace BasicDataTypes
 {
@@ -52,11 +53,27 @@ namespace BasicDataTypes
             }
             Console.WriteLine();
         }
+        static void UseBigInteger()
+        {
+            Console.WriteLine("=> Use BigInteger:");
+            BigInteger biggy =
+                BigInteger.Parse("99999999999999999999999999999999999999999999");
+            Console.WriteLine("Value of biggy is {0}", biggy);
+            Console.WriteLine("Is biggy an even value?: {0}", biggy.IsEven);
+            Console.WriteLine("Is biggy a power of two? {0}", biggy.IsPowerOfTwo);
+            BigInteger reallyBig = BigInteger.Multiply(biggy,
+                BigInteger.Parse("88888888888888888888888888888888888888888888"));
+            Console.WriteLine("Value of reallyBig is {0}", reallyBig);
+
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
             LocalVarDeclarations();
             DefaultDeclarations();
             ParsFromStringWithTryParse();
+            UseBigInteger();
+
             Console.ReadLine();
         }
     }
